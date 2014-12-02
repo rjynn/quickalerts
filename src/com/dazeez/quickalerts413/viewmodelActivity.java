dbdd1bb06fe2ac65e2a09d8646e05b8752f3aa71
iameric1986 <ec10@ualberta.ca>
2014-12-02 01:20:44 -0700

    Merging

--- src/com/dazeez/quickalerts413/viewmodelActivity.java
+++ src/com/dazeez/quickalerts413/viewmodelActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class viewmodelActivity extends Activity {
+	private int type = -1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,4 +19,38 @@ public class viewmodelActivity extends Activity {
 		getActionBar().setDisplayShowHomeEnabled(false);
 	}
 
+	public void zoom(View vew) {
+		ImageView image = (ImageView) findViewById(R.id.imageView1);
+		
+		if (this.type == -1) {
+			Toast.makeText(this, "Please select how the map will visualize data", Toast.LENGTH_SHORT).show();
+		} else if (this.type == 1) {
+			image.setImageDrawable(getResources().getDrawable(R.drawable.point));
+		} else if (this.type == 2) {
+			image.setImageDrawable(getResources().getDrawable(R.drawable.circle));
+		} else if (this.type == 3) {
+			image.setImageDrawable(getResources().getDrawable(R.drawable.heat));
+		}
+
+	}
+
+	public void onRadioButtonClicked(View view) {
+		boolean checked = ((RadioButton) view).isChecked();
+
+		switch (view.getId()) {
+		case R.id.point:
+			if (checked)
+				this.type = 1;
+			break;
+		case R.id.circle:
+			if (checked)
+				this.type = 2;
+			break;
+		case R.id.heat:
+			if (checked)
+				this.type = 3;
+			break;
+
+		}
+	}
 }

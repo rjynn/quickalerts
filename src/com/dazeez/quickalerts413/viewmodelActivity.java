b44a34eb887191252d4fb6b50b7c5e82c80377b3
Ruby <rjsdejesus@gmail.com>
2014-12-02 03:29:44 -0700

    changed logo and did stuff

--- src/com/dazeez/quickalerts413/viewmodelActivity.java
+++ src/com/dazeez/quickalerts413/viewmodelActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -19,6 +20,8 @@ public class viewmodelActivity extends Activity {
 		getActionBar().setDisplayShowTitleEnabled(false);
 		getActionBar().setDisplayShowHomeEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
+		Button btn = (Button) findViewById(R.id.button3);
+		btn.setEnabled(false);
 	}
 	
 	@Override

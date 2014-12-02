4c9317b90135995049ac106f1eec65d2429b2b51
Ruby <rjsdejesus@gmail.com>
2014-12-02 02:12:37 -0700

    added menu and home button

--- src/com/dazeez/quickalerts413/viewmodelActivity.java
+++ src/com/dazeez/quickalerts413/viewmodelActivity.java
@@ -3,6 +3,7 @@ package com.dazeez.quickalerts413;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class viewmodelActivity extends Activity {
@@ -12,6 +13,21 @@ public class viewmodelActivity extends Activity {
 		setContentView(R.layout.viewmodel);
 		getActionBar().setDisplayShowTitleEnabled(false);
 		getActionBar().setDisplayShowHomeEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, HomeActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			break;
+		}
+		
+		return (super.onOptionsItemSelected(item));
+	}
+	
 
 }
